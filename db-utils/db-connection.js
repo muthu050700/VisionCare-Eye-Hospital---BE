2ebd909d4b4ca84ec1d3e7c7156557ddee57e421
muthu050700 <muthukumaran050700@gmail.com>
2024-10-14 20:25:49 +0530

    enabled a cloud url

--- db-utils/db-connection.js
+++ db-utils/db-connection.js
@@ -7,7 +7,6 @@ const dbName = "visioncare-eye-hospital";
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0`;
 
 export const client = new mongodb.MongoClient(url);
-
 export const db = client.db(dbName);
 
 export const connectToDb = async () => {
